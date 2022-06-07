73c4223e5ae0f61680a1b387f817396426fd0414
fajrinam <73771452+fajrinam@users.noreply.github.com>
2022-06-07 17:45:22 +0700

    fix input form

--- laravel-app/app/Http/Controllers/FormController.php
+++ laravel-app/app/Http/Controllers/FormController.php
@@ -1,25 +1,28 @@
 <?php
  
-namespace App\Http\Controllers;
- 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
- 
-class FormController extends Controller
-{
-    public function input()
-    {
-        return view('input');
-    }
- 
-    public function proses(Request $request)
-    {
-        $this->validate($request,[
-           'nama' => 'required|min:5|max:20',
-           'pekerjaan' => 'required|alpha',
-           'usia' => 'required|numeric'
-        ]);
- 		
-        return view('proses',['data' => $request]);
-    }
-}
+ namespace App\Http\Controllers;
+  
+ use App\Http\Controllers\Controller;
+ use Illuminate\Http\Request;
+  
+ class FormController extends Controller
+ {
+     public function input()
+     {
+         return view('input');
+     }
+  
+     public function proses(Request $request)
+     {
+         $this->validate($request,[
+            'nama' => 'required|min:5|max:20',
+            'nrp' => 'required|numeric|min:5',
+            'usia' => 'required|numeric',
+            'alamat' => 'required|min:3|max:50',
+            'noHp' => 'required|numeric|min:3',
+            'gambar' => 'required'
+         ]);
+         return view('proses',['data' => $request]);
+         return redirect('/input')->withSuccess('Data berhasil disimpan!');
+     }
+ }
